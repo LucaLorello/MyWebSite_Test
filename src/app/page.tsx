c70a70704457e8b965eaa4eaff4ea7050246bcea
Luca Lorello <llorello98@MacBook-Pro-di-Luca.local>
2024-02-23 16:35:47 +0100

    Add initial website structure

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export interface IInfoPros {
+}
+
+export default function Info (pops: IInfoPros) {
+  return (
+      <div>    
+      click here    
+    </div>
+  );    
+}
+    
